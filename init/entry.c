a7229c2e1958d4eee5c4b6e11db78baacb90f75a
Yuchen Tang <twicy@umich.edu>
2023-08-09 20:18:21 +0800

    print phyisical layout

--- init/entry.c
+++ init/entry.c
@@ -4,6 +4,7 @@
 #include "idt.h"
 #include "debug.h"
 #include "timer.h"
+#include "pmm.h"
 
 int kern_entry()
 {
@@ -19,7 +20,13 @@ int kern_entry()
 	// asm volatile ("int $0x3");
 	// asm volatile ("int $0x21");
 	// asm volatile ("int $0x20");
-	asm volatile ("sti");
+	// asm volatile ("sti");
+
+	printk("kernel in memory start: 0x%08X\n", kern_start);
+	printk("kernel in memory end:   0x%08X\n", kern_end);
+	printk("kernel in memory used:   %d KB\n\n", (kern_end - kern_start) / 1024);
+	
+	show_memory_map();
 
 	return 0;
 }
